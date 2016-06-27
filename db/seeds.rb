312fdfd4d242fd83b7a1c8c0ab894d27692bc71d
Zach Morek <zmorek@gmail.com>
2016-06-27 16:41:51 -0400

    (m) comment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,15 @@
 # Development Drivers
 Driver.destroy_all
 [
-  {
+  { # Central point of NHV
     lat: 41.3083,
     lng: -72.9279,
     available: true,
-  },{
+  },{ # Near East Rock Park
     lat: 41.324668,
     lng: -72.9123427,
     available: true,
-  },{
+  },{ # SOM
     lat: 41.3154237,
     lng: -72.9209239,
     available: true,
